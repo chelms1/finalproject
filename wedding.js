3ab3f831dd99e1951debd84747558875d43fdf30
chelms1 <chelms1@stumail.jccc.edu>
2016-05-11 00:07:20 -0500

    Almost Done

--- wedding.js
+++ wedding.js
@@ -12,32 +12,93 @@ fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
   fjs.parentNode.insertBefore(js, fjs);
 }}(document, 'script', 'facebook-jssdk'));
-			
 	var map;
-	function initMap() {
+	var marker;
+	var marker2;
+	var marker3;
+	var BedBathPos = {lat:38.9288751, lng: -94.7838647};
+	var IkeaPos = {lat: 39.019538,lng: -94.7608667};
+	var TargetPos ={lat: 38.9288908,lng:-94.7488442};
+
+	function initMap(position) {
+	var latlon = position.coords.latitude + "," + position.coords.longitude;
 	  map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	  });
-	}
+		center: latlon,
+		zoom: 9
+		});
+	 marker = new google.maps.Marker({
+		position: BedBathPos,
+		map: map,
+		title:'Hello World'
+  });
+	marker2 = new google.maps.Marker({
+		position: IkeaPos,
+		map: map,
+		title:'Hello World'
+  });
+      	marker3 = new google.maps.Marker({
+		position: TargetPos,
+		map: map,
+		title:'Hello World'
+  });
+}
+	function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: 38.9168474, lng: -94.824447},
+	zoom: 9
+	});
+
+    var contentString1 = '<div id="content">'+
+      '<h1><a href="http://www.bedbathandbeyond.com/store/page/Registry">Bed Bath and Beyond</a><h1> '
+      '</div>';
+
+  var infowindow1 = new google.maps.InfoWindow({
+    content: contentString1
+  });
+  
+	var marker = new google.maps.Marker({
+    position: BedBathPos,
+    map: map,
+	title:'Bed Bath and Beyond'
+	});
 	
-var locationInfo = navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	  marker.addListener('click', function() {
+    infowindow1.open(map, marker);
+  });
+	    var contentString2 = '<div id="content">'+
+      '<h1><a href="http://info.ikea-usa.com/giftregistry/">Ikea</a></h1> '
+      '</div>';
 
-function onSuccess(locationObj){
-  console.log(locationObj);
-  var latitude=locationObj.coords.latitude;
-  console.log(latitude);
+  var infowindow2 = new google.maps.InfoWindow({
+    content: contentString2
+  });
   
-};
+  	marker2 = new google.maps.Marker({
+		position: IkeaPos,
+		map: map,
+		title:'Ikea'
+  });
+    marker2.addListener('click', function() {
+    infowindow2.open(map, marker2);
+  });
 
-function onError(error){
-  console.log(error);
-  
-};
+    var contentString = '<div id="content">'+
+      '<h1><a href="http://www.target.com/registry/wedding/find">Target</a><h1> '
+      '</div>';
 
-
-
-
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+    	marker3 = new google.maps.Marker({
+		position: TargetPos,
+		map: map,
+		title:'Target'
+  });
+  marker3.addListener('click', function() {
+    infowindow.open(map, marker3);
+  });
+}
+initMap();
 
 
 
